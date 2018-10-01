9c1fbb7878c9459166e82a4e27d1c77940446b7f
PackHg <pack.heng@gmail.com>
2018-10-01 19:02:01 +1000

    feat: Populate the UI of DetailActivity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -20,11 +20,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -59,7 +62,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -72,7 +75,69 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     * Populates the UI of {@link DetailActivity} with the given {@param sandwich} data.
+     * Does nothing if {@param sandwich} is null.
+     *
+     * @param sandwich a {@link Sandwich} object.
+     */
+    private void populateUI(Sandwich sandwich) {
 
+        if (sandwich == null) {
+            // Do nothing
+            return;
+        }
+
+        TextView alsoKnownAsTv = (TextView) findViewById(R.id.also_known_tv);
+        ArrayList<String> alsoKnownAs = (ArrayList<String>) sandwich.getAlsoKnownAs();
+        String s = toStringWithAnd(alsoKnownAs);
+        if (s != null) {
+            alsoKnownAsTv.setText(s);
+        }
+
+        TextView placeOfOriginTv = (TextView) findViewById(R.id.origin_tv);
+        placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+
+        TextView descriptionTv = (TextView) findViewById(R.id.description_tv);
+        descriptionTv.setText(sandwich.getDescription());
+
+        TextView ingredientsTv = (TextView) findViewById(R.id.ingredients_tv);
+        ArrayList<String> ingredients = (ArrayList<String>) sandwich.getIngredients();
+        s = toStringWithAnd(ingredients);
+        if (s != null) {
+            ingredientsTv.setText(s);
+        }
+    }
+
+    /**
+     * Returns a {@link String} in the form of "s1, s2, ... and sn" where s1, s2, ...
+     * and sn are the {@link String} items of the {@param stringArrayList}.
+     * Returns null if {@param stringArrayList} is null.
+     *
+     * @param stringArrayList a {@link ArrayList<String>}.
+     * @return {@link String}
+     */
+    private String toStringWithAnd(ArrayList<String> stringArrayList) {
+
+        if (stringArrayList == null) {
+            return null;
+        }
+
+        final String COMMA = ", ";
+        final String AND = " and ";
+        final String PERIOD = ".";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < stringArrayList.size(); i++) {
+            if (i == 0) {
+                stringBuilder.append(stringArrayList.get(i));
+            } else if (i < stringArrayList.size() - 1) {
+                stringBuilder.append(COMMA).append(stringArrayList.get(i));
+            } else {
+                stringBuilder.append(AND).append(stringArrayList.get(i)).append(PERIOD);
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
